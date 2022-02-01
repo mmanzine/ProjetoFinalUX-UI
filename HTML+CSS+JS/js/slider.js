fd4fcbe6e2ab2320c22082e5d3d9472c3da4417b
Mario Augusto Manzine Junior <mario.manzine@hotmail.com>
2022-02-01 16:16:15 -0300

    background image updated

--- HTML+CSS+JS/js/slider.js
+++ HTML+CSS+JS/js/slider.js
@@ -1,12 +1,20 @@
 //Portfolio slider
 
-  //Declarando as variáveis do slider
+  //declarando as variáveis do slider
 
   var sliderContainer = document.querySelector('.mm-slider-container');
   var sliderList = document.querySelector('.mm-slider-list');
   var sliderItem = document.querySelectorAll('.mm-slider-item');
+  const sliderTotalItems = sliderItem.length;
   var sliderListWidth = null;
+  var prevItem = document.querySelector('.mm-item-prev');
+  var nextItem = document.querySelector('.mm-item-next');
+  var sliderPos = 0; 
+  var currentSlide = document.querySelector ('.mm-current-slide');
+  var totalSlide = document.querySelector ('.mm-total-slide');
+  var currentCounter = 1;
 
+  
 //capturando larguras individuais
   var containerWidth = sliderContainer.parentElement.offsetWidth;
     
@@ -18,46 +26,72 @@
   
       var sliderItemWidth = sliderItem[p].offsetWidth;
   
-      sliderListWidth += sliderItemWidth;
-      
+      sliderListWidth += sliderItemWidth;      
   }
   
   sliderList.style.width = sliderListWidth +'px';
-  
-  
-//fazendo a animação do Slider onClick
-    var prevItem = document.querySelector('.mm-item-prev');
-    var nextItem = document.querySelector('.mm-item-next');
-    var sliderPos = 0; 
+
+//handlers 
+
+var nextSlideAnime = function(){
+    var lastItem = sliderListWidth - containerWidth;
+    if (-1*(sliderPos)===lastItem) {
+        return;
+    }
+    sliderPos -= containerWidth;
     
+    anime({
+        targets: sliderList,
+        translateX: sliderPos
+      });    
+  }
+
+  var prevSlideAnime = function(){
+    if (sliderPos===0) {
+        return;
+    }
+    sliderPos += containerWidth;
+        anime({
+        targets: sliderList,
+        translateX: sliderPos
+      });
+  }
+
+    //counter formatter
+    var counterFormatter = function(n){
+        if(n < 10){
+            return'0'+n;
+        } else {
+            return n;
+        }
+    }
+
+    //counter add
+    var counterAdd = function(){
+        if(currentCounter >=0 && currentCounter < sliderTotalItems){
+        currentCounter++;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+        }
+    }
+
+       //counter remove
+       var counterRemove = function(){
+        if(currentCounter > 1 && currentCounter <= sliderTotalItems){
+        currentCounter--;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+        }
+    }
+
+    //actions
+    totalSlide.innerHTML = counterFormatter (sliderTotalItems);
+    
+
     nextItem.addEventListener ('click', function(){
-      var lastItem = sliderListWidth - containerWidth;
-      if (-1*(sliderPos)===lastItem) {
-          return;
-      }
-  
-      sliderPos -= containerWidth;
-      
-  
-      anime({
-          targets: sliderList,
-          translateX: sliderPos
-        });
-      
-    })
-  
+    nextSlideAnime()
+    counterAdd()    
+    });
+       
     prevItem.addEventListener ('click', function(){
-    
-      if (sliderPos===0) {
-          return;
-      }
-  
-      sliderPos += containerWidth;
-      
-  
-      anime({
-          targets: sliderList,
-          translateX: sliderPos
-        });
-        
-    })
+    prevSlideAnime()
+    counterRemove()         
+    });
